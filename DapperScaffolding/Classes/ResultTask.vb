2e935c723ddb367c1a80b08ec649ce3e74ef2670
Roberto Faccani <r.faccani@outlook.it>
2026-02-10 01:28:35 +0100

    Added result codes

--- DapperScaffolding/Classes/ResultTask.vb
+++ DapperScaffolding/Classes/ResultTask.vb
@@ -1,7 +1,24 @@
 ﻿Public Class ResultTask
 
-    Public Property isSuccess As Boolean
-    Public Property ErrorMessage As String
-    Public Property Exception As Exception
+    Public Const TASK_SUCCESS As Integer = 0
+    Public Const TASK_APPSETTINGNOTFOUND As Integer = 1
+    Public Const TASK_CONNECTIONSTRINGNOTFOUND As Integer = 2
+    Public Const TASK_EXCEPTION As Integer = 3
+    Public Const TASK_PROJECTNOTFOUND As Integer = 4
+    Public Const TASK_ARGUMENTSNOTVALID As Integer = 5
+    Public Const TASK_NOTIMPLMENTED As Integer = 6
+    Public Const TASK_UNEXPECTED As Integer = 99
+
+    Public ReadOnly Property isSuccess As Boolean
+    Public ReadOnly Property ErrorMessage As String
+    Public ReadOnly Property Exception As Exception
+    Public ReadOnly Property ReturnCode As Integer
+
+    Public Sub New(returnCode As Integer, Optional errorMessage As String = "", Optional ex As Exception = Nothing)
+        Me.ReturnCode = returnCode
+        Me.ErrorMessage = errorMessage
+        Me.Exception = ex
+        Me.isSuccess = (returnCode = TASK_SUCCESS)
+    End Sub
 
 End Class
